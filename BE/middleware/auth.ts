02341a6cbd69b97bd320bdff02be4efb88ad2ece
Soumya Ghosh <soumyaidmail@gmail.com>
2024-01-11 23:00:05 +0530

    auth refactored

--- BE/middleware/auth.ts
+++ BE/middleware/auth.ts
@@ -1,29 +1,24 @@
-import jwt, { JwtPayload } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import { Request, Response, NextFunction } from 'express'
 
-
-export interface CustomRequest extends Request {
- token: string | JwtPayload
-}
-
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
-const secretKey = process.env.JWT_SECRET_KEY
- try {
-   const token = req.header('Authorization')?.replace('Bearer ', '')
+  const secretKey = process.env.JWT_SECRET_KEY
+  try {
+    const token = req.header('Authorization')?.replace('Bearer ', '')
 
-   if (!token) {
-     throw new Error()
-   }
+    if (!token) {
+      throw new Error()
+    }
+    const verified = jwt.verify(token, secretKey);
+    if (!verified) {
+      throw new Error()
+    }
 
-   console.log(token, secretKey)
-   const decoded = jwt.verify(token, secretKey);
-   (req as CustomRequest).token = decoded
-
-   next()
- } catch (err) {
+    next()
+  } catch (err) {
     res.json({
-        status: 401,
-        message: "Please Authenticate!",
-      }).send()
- }
+      status: 401,
+      message: "Please Authenticate!",
+    }).send()
+  }
 }
